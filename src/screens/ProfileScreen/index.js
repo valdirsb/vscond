20cc6707908384cb900e3dad89abf9da150229d7
vasantecnologia <vasan.tecnologia@gmail.com>
2021-08-01 19:46:50 -0300

    atualização da tela PERFIL

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,26 +1,97 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { launchCamera } from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import C from './style';
 
 import { useStateValue } from '../../contexts/StateContext';
 import api from '../../services/api';
 
-import DocItem from '../../components/DocItem';
+
 
 export default () => {
     const navigation = useNavigation();
+    const [context, dispatch] = useStateValue();
 
-
+    const [photo, setPhoto] = useState({});
+    const [description, setDescription] = useState('');
+    const [where, setWhere] = useState('');
+    
     useEffect(()=>{
         navigation.setOptions({
-            headerTitle: 'Perfil'
+            headerTitle: 'Meu Perfil'
         });
     }, []);
 
+    const handleAddPhoto = () => {
+        launchCamera({
+            mediaType: 'photo',
+            maxHeight: 900,
+            maxWidth: 1200,
+            quality:0.8
+        }, (response) => {
+            if(!response.didCancel) {
+                setPhoto(response.assets[0]);
+            }
+        });
+    }
 
+    const handleAddLibrary = () => {
+        launchImageLibrary({
+            mediaType: 'photo',
+            maxHeight: 600,
+            maxWidth: 800,
+            quality:0.7
+        }, (response) => {
+            if(!response.didCancel) {
+                setPhoto(response);
+            }
+        });
+    }
+
+    const handleSave = async () => {
+        if('' && photo.uri !== '') {
+            const result = await api.addLostItem(
+                photo, 
+            );
+            if(result.error === '') {
+                setPhoto({});
+                setDescription('');
+                setWhere('');
+                navigation.navigate('FoundAndLostScreen');
+            } else {
+                alert(result.error);
+            }
+        } else {
+            alert('Preencha os campos');
+        }
+    }
+
+
+   
     return (
         <C.Container>
-            <C.Titulo>Perfil - mudando...</C.Titulo>
+            <C.Title>Olá {context.user.user.name}</C.Title>
+            <C.Image
+                source={require('../../assets/undraw_home.png')}
+                resizeMode="contain"
+            />
+                <C.PhotoArea>
+                    {!photo.uri &&
+                        <C.ButtonArea onPress={handleAddPhoto}>
+                            <C.ButtonText>Tirar uma foto</C.ButtonText>
+                        </C.ButtonArea>
+                    }
+                    {photo.uri &&
+                        <>
+                            <C.PhotoItem source={{uri: photo.uri}} resizeMode="cover" />
+                            <C.ButtonArea onPress={handleAddPhoto}>
+                                <C.ButtonText>Tirar outra foto</C.ButtonText>
+                            </C.ButtonArea>
+                        </>
+                    }
+                </C.PhotoArea>
+            
         </C.Container>
     );
 }
--- src/screens/ProfileScreen/style.js
+++ src/screens/ProfileScreen/style.js
@@ -3,20 +3,81 @@ import styled from 'styled-components/native';
 export default {
     Container: styled.SafeAreaView`
         flex: 1;
+        padding: 20px;
         background-color: #F5F6FA;
+    `,
+    Scroller: styled.ScrollView`
+        flex: 1;
         padding: 20px;
     `,
-    LoadingIcon: styled.ActivityIndicator``,
-    NoListArea: styled.View`
+    PhotoArea: styled.View`
+        margin-bottom: 30px;
+    `,
+    PhotoScroll: styled.ScrollView`
+    flex: 1;
+    margin-bottom: 30px;
+`,
+PhotoAddButton: styled.TouchableOpacity`
+        width: 65px;
+        height: 65px;
         justify-content: center;
         align-items: center;
-        padding: 30px;
+        border-width: 1px;
+        border-color: #CCC;
+        border-radius: 5px;
+        margin-left: 5px;
     `,
-    Titulo: styled.Text`
-        font-size: 15px;
+    PhotoItem: styled.View`
+    width: 65px;
+    border-width: 1px;
+    border-color: #CCC;
+    border-radius: 5px;
+    padding-bottom: 5px;
+    margin-left: 5px;
+    align-items: center;
+`,
+Photo: styled.Image`
+    width: 63px;
+    height: 63px;
+    margin-bottom: 5px;
+    border-radius: 5px;
+`,
+PhotoRemoveButton: styled.TouchableOpacity``,
+    Image: styled.Image`
+        border-width: 2px;
+        border-color: #CCC;
+        background-color: #FFF;
+        border-radius: 15px;
+        width: 150px;
+        height: 150px;        
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 20px;
+    `,
+    PhotoArea: styled.View``,
+    Title: styled.Text`
+        font-size: 17px;
         color: #000;
+        margin: 10px 0;
     `,
-    List: styled.FlatList`
-        flex: 1;
+    Title: styled.Text`        
+        color: #000;
+        font-size: 15px;
+        padding: 10px;
+        margin-bottom: 15px;
+        align-items: center;
+    `,
+    ButtonArea: styled.TouchableOpacity`
+        background-color: #0098DA;
+        padding: 12px;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        margin-bottom: 15px;
+    `,
+    ButtonText: styled.Text`
+        color: #FFF;
+        font-size: 15px;
+        font-weight: bold;
     `
 };
